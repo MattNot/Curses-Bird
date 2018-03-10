2f17ceb07d69af0b6b4b91da80729a7cf5751f71
Matteo Notaro <matteoatnotaro@outlook.it>
2018-03-10 20:20:28 +0100

    pipe

--- Pipe.h
+++ Pipe.h
@@ -1,7 +1,18 @@
+#include "Bird.h"
 class Pipe
 {
     private:
         int top;
         int bot;
-        
+        int speed;
+    public:
+        void update();
+        void show();
+        bool isHit(Bird);
+        Pipe();
 };
+
+bool Pipe::isHit(Bird)
+{
+    if(this->)
+}
